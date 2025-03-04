b8c21ce0c8edd7b99898e4f61553d7be6c817dc6
julisr <julietarpo3@gmail.com>
2025-03-03 22:00:36 -0300

    1
    JS Y CSS

--- main.js
+++ main.js
@@ -1,22 +1,58 @@
+const formulario = document.querySelector(".formulario");
+const inputCancion = document.querySelector(".inputCancion"); 
+const inputArtista = document.querySelector(".inputArtista"); 
+const listaCanciones = document.querySelector(".canciones");  
 
-const formulario = document.querySelector(".formulario"); 
-const input = document.querySelector(".input"); 
-const listaCanciones = document.querySelector(".canciones"); 
+function agregar(event) {
+    event.preventDefault(); 
+
+    if (inputCancion.value.trim() && inputArtista.value.trim()) { 
+        let nombreCancion = document.createElement('li');
+        nombreCancion.classList.add('cancion');
+
+        
+        let texto = document.createElement("p");
+        texto.innerHTML = `<strong>${inputCancion.value}</strong> - ${inputArtista.value}`; 
+        nombreCancion.appendChild(texto);
+
+        
+        let icono = document.createElement("div");
+        icono.classList.add('iconos');
+        nombreCancion.appendChild(icono);
+
+        
+        let eliminar = document.createElement('i');
+        eliminar.classList.add('bi', 'bi-dash-circle');
+        eliminar.addEventListener('click', eliminarCancion);
+        icono.appendChild(eliminar);
+
+        listaCanciones.appendChild(nombreCancion);
+        
+        
+        inputCancion.value = "";
+        inputArtista.value = "";
+    } else {
+        alert('Por favor, ingresá la canción y el artista');
+    }
+}
+
+function eliminarCancion(e) {
+    let tarea = e.target.closest('li'); 
+    if (tarea) tarea.remove();
+}
 
 
-const agregar = event => {
-    event.preventDefault();
-    const nombreCancion = input.value.trim(); 
-    if (nombreCancion === "") return; 
-
-    const nuevaCancion = document.createElement("li"); 
-    nuevaCancion.textContent = nombreCancion; 
-
-    listaCanciones.appendChild(nuevaCancion); 
-    inputCancion.value = ""; 
-};
+formulario.addEventListener('submit', agregar);
 
 
-formulario.addEventListener("submit", agregar);
+inputCancion.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        agregar(e);
+    }
+});
 
-
+inputArtista.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        agregar(e);
+    }
+});
